4fbcc8934e2baa152ec5ad02c1cca4539af55c2d
expnkx <euloanty@live.com>
2020-07-08 16:18:52 -0400

    prounications error. It should be extent, not extend for ospan.

--- include/fast_io_core_impl/ospan.h
+++ include/fast_io_core_impl/ospan.h
@@ -4,12 +4,12 @@
 namespace fast_io
 {
 
-template<std::integral ch_type,std::size_t extend=std::dynamic_extent>
+template<std::integral ch_type,std::size_t extent=std::dynamic_extent>
 class ospan
 {
 public:
 	using char_type = ch_type;
-	using span_type = std::span<char_type,extend>;
+	using span_type = std::span<char_type,extent>;
 	span_type span;
 	using pointer = span_type::pointer;
 	pointer current;
@@ -31,39 +31,39 @@ public:
 	constexpr void clear() noexcept {current=span.data();}
 };
 
-template<std::integral char_type,std::size_t extend>
-inline constexpr auto obuffer_begin(ospan<char_type,extend>& sp) noexcept
+template<std::integral char_type,std::size_t extent>
+inline constexpr auto obuffer_begin(ospan<char_type,extent>& sp) noexcept
 {
 	return sp.data();
 }
 
-template<std::integral char_type,std::size_t extend>
-inline constexpr auto obuffer_curr(ospan<char_type,extend>& sp) noexcept
+template<std::integral char_type,std::size_t extent>
+inline constexpr auto obuffer_curr(ospan<char_type,extent>& sp) noexcept
 {
 	return sp.current;
 }
 
-template<std::integral char_type,std::size_t extend>
-inline constexpr auto obuffer_end(ospan<char_type,extend>& sp) noexcept
+template<std::integral char_type,std::size_t extent>
+inline constexpr auto obuffer_end(ospan<char_type,extent>& sp) noexcept
 {
 	return std::to_address(sp.span.end());
 }
 
-template<std::integral char_type,std::size_t extend>
-inline constexpr auto obuffer_set_curr(ospan<char_type,extend>& sp,char_type* ptr) noexcept
+template<std::integral char_type,std::size_t extent>
+inline constexpr auto obuffer_set_curr(ospan<char_type,extent>& sp,char_type* ptr) noexcept
 {
 	sp.current=ptr;
 }
 
-template<std::integral char_type,std::size_t extend>
-inline constexpr auto overflow(ospan<char_type,extend>&,char_type) noexcept
+template<std::integral char_type,std::size_t extent>
+inline constexpr auto overflow(ospan<char_type,extent>&,char_type) noexcept
 {
 	fast_terminate();
 }
 
-template<std::integral char_type,std::size_t extend,std::contiguous_iterator Iter>
+template<std::integral char_type,std::size_t extent,std::contiguous_iterator Iter>
 requires (std::same_as<char_type,std::iter_value_t<Iter>>||std::same_as<char,char_type>)
-inline constexpr void write(ospan<char_type,extend>& ob,Iter cbegin,Iter cend) noexcept	//contract : cend-begin + curr <= span's size
+inline constexpr void write(ospan<char_type,extent>& ob,Iter cbegin,Iter cend) noexcept	//contract : cend-begin + curr <= span's size
 {
 	if constexpr(std::same_as<char_type,std::iter_value_t<Iter>>)
 	{
