dd5065a46776b3e3e90a7192cec588af21b37942
ejsvifq_mabmip <euloanty@live.com>
2020-07-11 03:26:00 -0400

    io_uring code change first

--- examples/0058.io_uring/io_uring.cc
+++ examples/0058.io_uring/io_uring.cc
@@ -1,14 +1,25 @@
 #include"../../include/fast_io.h"
 #include"../../include/fast_io_device.h"
 #include"../../include/fast_io_driver/liburing.h"
+#include<coroutine>
+#include"../../include/fast_io_hosted/async_coro.h"
+
+inline fast_io::task io_task(fast_io::io_uring_observer ior)
+{
+	fast_io::onative_file nv("test.txt");
+	std::array<fast_io::io_scatter_t,1> scatter{{const_cast<char*>("Hello World\n"),12}};
+	co_await fast_io::async_scatter_write(ior,nv,scatter);
+	
+}
 
 int main()
 {
 	fast_io::io_uring ior(fast_io::io_async);
-	fast_io::onative_file onf("io_uring.txt");
-	std::array<fast_io::io_scatter_t,1> scatters{fast_io::io_scatter_t{const_cast<char*>("Hello World"),11}};
-	async_scatter_write_callback(ior,onf,scatters,[]
+	std::vector<std::jthread> pools;
+	pools.reserve(std::thread::hardware_concurrency());
+	pools.emplace_back([&](
 	{
-		::debug_print("succ!");
+		fast_io::linux::single_thread_io_async_scheduling(ior);
 	});
+	io_task(ior);
 }
