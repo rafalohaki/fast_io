637cac2648dc3d1b3ff8aa27c55a7cece311bf15
zyddnys <zyddnys@outlook.com>
2020-07-30 16:38:19 -0400

    gmac fix

--- include/fast_io_crypto/gmac.h
+++ include/fast_io_crypto/gmac.h
@@ -113,7 +113,7 @@ struct gmac
 			uint64_t *HiL = &HL[i], *HiH = &HH[i];
 			vh = *HiH;
 			vl = *HiL;
-			for( j = 1; j < i; j++ ) {
+			for( j = 1; j < i; ++j ) {
 				HiH[j] = vh ^ HH[j];
 				HiL[j] = vl ^ HL[j];
 			}
