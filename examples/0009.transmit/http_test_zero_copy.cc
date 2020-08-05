bdc830f3ceb3660c4a358b07740504da92635298
expnkx <euloanty@live.com>
2020-08-05 02:27:51 -0400

    avoid using any real website for examples. especially no https ones

--- examples/0009.transmit/http_test_zero_copy.cc
+++ examples/0009.transmit/http_test_zero_copy.cc
@@ -7,7 +7,7 @@ int main()
 try
 #endif
 {
-	fast_io::ibuf_tcp_client hd(fast_io::dns_once("www.jszg.edu.cn"),80);
+	fast_io::ibuf_tcp_client hd(fast_io::dns_once("xxxx"),80);
 	fast_io::inative_file request_file("http_request.txt");
 	transmit(hd,request_file);
 	skip_http_header(hd);
