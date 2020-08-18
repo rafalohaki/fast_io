18a20ba0cadf969214f78688f655a4c8793c1f3a
ejsvifq_mabmip <euloanty@live.com>
2020-08-18 07:54:39 -0400

    The value of FILE_ATTRIBUTE_TEMPORARY is 256

--- include/fast_io_hosted/platforms/win32.h
+++ include/fast_io_hosted/platforms/win32.h
@@ -276,7 +276,7 @@ does not exist
 	if((value&open_mode::temporary)!=open_mode::none)
 	{
 		mode.dwFlagsAndAttributes|=0x04000000;					//FILE_FLAG_DELETE_ON_CLOSE
-//		mode.dwFlagsAndAttributes|=0x2;						To verify: FILE_FLAG_TEMPORARY
+		mode.dwFlagsAndAttributes|=0x100;					//FILE_ATTRIBUTE_TEMPORARY
 	}
 	return mode;
 }
