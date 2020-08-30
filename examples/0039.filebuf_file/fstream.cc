e2a65f2523bb36409a6b1baf2771afd0ae1d800c
ejsvifq_mabmip <euloanty@live.com>
2020-08-30 14:03:02 -0400

    fstream

--- examples/0039.filebuf_file/fstream.cc
+++ examples/0039.filebuf_file/fstream.cc
@@ -7,8 +7,9 @@ int main()
 	fast_io::filebuf_file fb("ofstream.txt",fast_io::open_mode::binary|fast_io::open_mode::out);
 	std::ofstream out;
 	*out.rdbuf()=std::move(*fb.native_handle());
+	fast_io::filebuf_io_observer fiob{out.rdbuf()};
 	out<<"Hello world from std::ofstream\n";
-	print(fb,"Hello World from fast_io\n");
+	print(fiob,"Hello World from fast_io\n");
 	out<<"Hello world from std::ofstream again\n";
-	print(fb,"Hello World from fast_io again\n");
+	print(fiob,"Hello World from fast_io again\n");
 }
