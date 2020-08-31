a6fb31cc5653b17046ef191638ba871d21a352f1
ejsvifq_mabmip <euloanty@live.com>
2020-08-31 17:36:35 -0400

    directory example for POSIX conforms systems

---
+++ examples/0073.filesystem/directory.cc
@@ -0,0 +1,9 @@
+#include"../../include/fast_io.h"
+#include"../../include/fast_io_device.h"
+
+int main()
+{
+	fast_io::directory dir(".");
+	fast_io::obuf_file obf(fast_io::io_at,dir,"hello_directory.txt");
+	print(obf,"Hello World\n");
+}
--- include/fast_io_hosted/filesystem/native.h
+++ include/fast_io_hosted/filesystem/native.h
@@ -3,3 +3,16 @@
 #if _BSD_SOURCE || _SVID_SOURCE || (_POSIX_C_SOURCE >= 200809L || _XOPEN_SOURCE >= 700)
 #include"posix_directory.h"
 #endif
+
+
+namespace fast_io
+{
+
+#if _BSD_SOURCE || _SVID_SOURCE || (_POSIX_C_SOURCE >= 200809L || _XOPEN_SOURCE >= 700)
+using native_directory_io_observer = posix_directory_io_observer;
+using native_directory_io_handle = posix_directory_io_handle;
+using native_directory_file = posix_directory_file;
+using directory = native_directory_file;
+#endif
+
+}
