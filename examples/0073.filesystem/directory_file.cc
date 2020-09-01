c0d5684ba366719e9332a79c727e79353a7b3cf4
ejsvifq_mabmip <euloanty@live.com>
2020-09-01 04:47:17 -0400

    no need to include<vector> in directory_file.cc example

--- examples/0073.filesystem/directory_file.cc
+++ examples/0073.filesystem/directory_file.cc
@@ -1,4 +1,3 @@
-#include<vector>
 #include"../../include/fast_io.h"
 #include"../../include/fast_io_device.h"
 
@@ -7,7 +6,7 @@ int main()
 	fast_io::directory dir(".");
 	for(auto const& e: recursive(dir))
 	{
-		if(type(e)==fast_io::file_type::regular&&extension(e)==".cc")
+		if(type(e)==fast_io::file_type::regular&&extension(e)=="cc")
 		{
 			fast_io::inative_file infl(fast_io::at,e,filename(e));
 			transmit(fast_io::out(),infl);
