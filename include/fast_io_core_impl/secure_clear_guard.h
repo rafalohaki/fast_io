83d5a7c8ffcffaff31fc7fe06c03dfc0e51da6e7
ejsvifq_mabmip <euloanty@live.com>
2020-09-16 15:34:01 -0400

    addd https://www.usenix.org/sites/default/files/conference/protected-files/usenixsecurity17_slides_zhaomo_yang.pdf secure clear paper
    to code

--- include/fast_io_core_impl/secure_clear_guard.h
+++ include/fast_io_core_impl/secure_clear_guard.h
@@ -10,6 +10,39 @@ extern "C" void __stdcall RtlSecureZeroMemory(void*,std::size_t) noexcept;
 #endif
 }
 
+/*
+Based on security paper Dead Store Elimination (Still) Considered Harmful
+
+https://www.usenix.org/system/files/conference/usenixsecurity17/sec17-yang.pdf
+
+https://www.usenix.org/sites/default/files/conference/protected-files/usenixsecurity17_slides_zhaomo_yang.pdf
+
+Existing Technique: Using memory barrier
+GCC supports a memory barrier expressed using an inline assembly statement.
+According to GCC’s documentation, the clobber argument "memory" tells the
+compiler that the inline assembly statement may read or write memory that is not
+specified in the input or output arguments.
+
+Used in: zap from Kerberos, memzero_explicit from Linux.
+Availability: GCC and Clang.
+Effectiveness: effective
+Not effective on Clang:
+	std::memset(data,0,size);
+	__asm__ __volatile__("" ::: "memory");
+
+
+Effective on Clang
+	std::memset(data,0,size);
+	__asm__ __volatile__("" ::"r"(data): "memory");
+
+How difficult to create a reliable scrubbing function
+However, it does not work with Clang.
+A more reliable and portable memory barrier is shown below (which is also used
+used in memzero_explicit):
+
+
+*/
+
 inline void secure_clear(void* data,std::size_t size)
 {
 #if defined(_MSC_VER)
@@ -18,6 +51,7 @@ inline void secure_clear(void* data,std::size_t size)
 /*
 https://github.com/bminor/glibc/blob/master/string/explicit_bzero.c
 Referenced from glibc
+
 */
 	std::memset(data,0,size);
 	__asm__ __volatile__("" ::"r"(data): "memory");
