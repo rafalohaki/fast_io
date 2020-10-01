c6f1ae068bb31273bc59c4e547e3d20626192aae
ejsvifq_mabmip <euloanty@live.com>
2020-10-01 16:43:07 -0400

    exceptions should be aliased to char const*

--- include/fast_io_freestanding_impl/exception.h
+++ include/fast_io_freestanding_impl/exception.h
@@ -2,14 +2,9 @@
 
 namespace fast_io
 {
-/*We have no choice but to assume it is ansi based. Even On EBCDIC systems
-since we cannot prevent people link from other libraries using ANSI that throw exception*/
 
-template<output_stream output>
-requires std::same_as<typename output::char_type,char>
-inline void print_define(output& out,std::exception const &e)
+inline basic_io_scatter_t<char> print_alias_define(io_alias_t,std::exception const &e)
 {
-	print_freestanding(out,chvw(e.what()));
+	return {e.what(),strlen(e.what())};	
 }
-
 }
