f7cf44d0557c2f7b83d4c65015e777204f91218e
ejsvifq_mabmip <euloanty@live.com>
2020-10-04 06:46:14 -0400

    no perfect forwarding for mfc open_mode

--- include/fast_io_driver/mfc.h
+++ include/fast_io_driver/mfc.h
@@ -123,10 +123,10 @@ public:
 		hd.release();
 	}
 	basic_mfc_file(cstring_view file,open_mode om,perms pm=static_cast<perms>(436)):
-		basic_mfc_file(basic_win32_file<char_type>(file,om,std::forward<Args>(args)...),om)
+		basic_mfc_file(basic_win32_file<char_type>(file,om,pm),om)
 	{}
 	basic_mfc_file(native_at_entry nate,cstring_view file,open_mode om,perms pm=static_cast<perms>(436)):
-		basic_mfc_file(basic_win32_file<char_type>(nate,file,om,std::forward<Args>(args)...),om)
+		basic_mfc_file(basic_win32_file<char_type>(nate,file,om,pm),om)
 	{}
 	basic_mfc_file(basic_mfc_file const&)=default;
 	basic_mfc_file& operator=(basic_mfc_file const&)=default;
