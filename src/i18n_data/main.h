9f743dbf3db38bf77b3f6e406b86df4403af8663
ejsvifq_mabmip <euloanty@live.com>
2020-12-31 02:52:35 -0500

    use chinese character "我" to detect whether execution charset is UTF-8 or not. if it is not, we suppose it is GB18030. emoji has issues since microsoft does not seem to correctly support UTF-8 to GB18030 codecvt.

--- src/i18n_data/main.h
+++ src/i18n_data/main.h
@@ -25,17 +25,17 @@ export_locale_data(lc_locale* lc_ptr) noexcept
 {
 	using lc_all_ptr [[gnu::may_alias]] = lc_all const*;
 	lc_all_ptr ptr;
-	if constexpr(compile_time_compare("😀",u8"😀"))
+	if constexpr(compile_time_compare("我",u8"我"))
 		ptr=reinterpret_cast<lc_all_ptr>(&u8lc_all_global);
 	else
 		ptr=&lc_all_global;
 	using wlc_all_ptr [[gnu::may_alias]] = wlc_all const*;
 	wlc_all_ptr wptr;
-	if constexpr(compile_time_compare(u8"😀",L"😀"))
+	if constexpr(compile_time_compare(u8"我",L"我"))
 		wptr=reinterpret_cast<wlc_all_ptr>(&u8lc_all_global);
-	else if constexpr(compile_time_compare(u"😀",L"😀"))
+	else if constexpr(compile_time_compare(u"我",L"我"))
 		wptr=reinterpret_cast<wlc_all_ptr>(&u16lc_all_global);
-	else if constexpr(compile_time_compare(U"😀",L"😀"))
+	else if constexpr(compile_time_compare(U"我",L"我"))
 		wptr=reinterpret_cast<wlc_all_ptr>(&u32lc_all_global);
 	else
 		wptr=&wlc_all_global;
