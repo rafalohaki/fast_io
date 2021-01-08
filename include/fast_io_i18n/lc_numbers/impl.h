ecbbe42e866b5d1afe06ffd356af34dd0b8cfdc7
ejsvifq_mabmip <euloanty@live.com>
2021-01-07 23:00:12 -0500

    use macro to guard against clang #if __cpp_lib_chrono >= 201907L || __GLIBCXX__ >= 20201001

--- include/fast_io_i18n/lc_numbers/impl.h
+++ include/fast_io_i18n/lc_numbers/impl.h
@@ -2,6 +2,6 @@
 
 #include"bool_message.h"
 #include"grouping.h"
-#if __cpp_lib_chrono >= 201907L || __GNUC__>= 11
+#if __cpp_lib_chrono >= 201907L || __GLIBCXX__ >= 20201001
 #include"chrono.h"
 #endif
