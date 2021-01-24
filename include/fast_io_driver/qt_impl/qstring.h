45b10954e449a6a2bd021eb551e92b884f2e4e8f
ejsvifq_mabmip <euloanty@live.com>
2021-01-24 08:49:07 -0500

    Many companies still use older versions of Qt. We avoid using QStringView

--- include/fast_io_driver/qt_impl/qstring.h
+++ include/fast_io_driver/qt_impl/qstring.h
@@ -2,75 +2,81 @@
 
 namespace fast_io
 {
+
+namespace details
+{
+struct qt_scatter
+{
+	using char16_ptr_mayalias
+#if __has_cpp_attribute(gnu::may_alias)
+	[[gnu::may_alias]]
+#endif
+	= char16_t const*;
+	char16_ptr_mayalias base{};
+	std::size_t len{};
+};
+}
+
 namespace manipulators
 {
-
+#if defined(QT_VERSION) && defined(QT_VERSION_CHECK)
+#if (QT_VERSION <= QT_VERSION_CHECK(5,6,0))
 template<typename T>
 requires std::same_as<std::remove_cvref_t<T>,QStringView>
-inline constexpr cdcvt_t<QStringView> cdcvt(T t) noexcept
+inline constexpr cdcvt_t<::fast_io::details::qt_scatter> cdcvt(T t) noexcept
 {
-	return {t};
+	if constexpr(sizeof(qsizetype)>sizeof(std::size_t))
+	{
+		if(t.size()>SIZE_MAX)
+			fast_terminate();
+	}
+	return {reinterpret_cast<::fast_io::details::qt_scatter::char16_ptr_mayalias>(t.data()),static_cast<std::size_t>(t.size())};
 }
+
+#endif
+#endif
 
 template<typename T>
 requires (std::same_as<std::remove_cvref_t<T>,QString>||
 	std::same_as<std::remove_cvref_t<T>,QStringRef>)
-inline constexpr cdcvt_t<QStringView> cdcvt(T&& t) noexcept
+inline constexpr cdcvt_t<::fast_io::details::qt_scatter> cdcvt(T&& t) noexcept
 {
-	return {t};
+	if constexpr(sizeof(qsizetype)>sizeof(std::size_t))
+	{
+		if(t.size()>SIZE_MAX)
+			fast_terminate();
+	}
+	return {reinterpret_cast<::fast_io::details::qt_scatter::char16_ptr_mayalias>(t.data()),static_cast<std::size_t>(t.size())};
 }
-
 }
 
 namespace details
 {
 template<std::integral char_type>
-inline constexpr std::size_t print_reserve_size_qstring_view_impl(QStringView vw) noexcept
+inline constexpr std::size_t print_reserve_size_qt_scatter_impl(qt_scatter vw) noexcept
 {
 	if constexpr(exec_charset_is_ebcdic<char_type>())
 		fast_terminate();
-	if constexpr(sizeof(qsizetype)==sizeof(std::size_t))
+	if constexpr(sizeof(char_type)==1)
 	{
-		if constexpr(sizeof(char_type)==1)
-		{
-			if((SIZE_MAX>>2)<vw.size())
-				fast_terminate();
-			return static_cast<std::size_t>(vw.size())<<2;
-		}
-		else
-			return static_cast<std::size_t>(vw.size());
+		if((SIZE_MAX>>2)<vw.len)
+			fast_terminate();
+		return static_cast<std::size_t>(vw.len);
 	}
 	else
 	{
-
-		if constexpr(sizeof(char_type)==1)
-		{
-			if((SIZE_MAX>>2)<vw.size())
-				fast_terminate();
-			return static_cast<std::size_t>(vw.size());
-		}
-		else
-		{
-			if(SIZE_MAX<vw.size())
-				fast_terminate();
-			return static_cast<std::size_t>(vw.size());
-		}
+		if(SIZE_MAX<vw.len)
+			fast_terminate();
+		return static_cast<std::size_t>(vw.len);
 	}
 }
 
 template<std::random_access_iterator Iter>
-inline constexpr Iter print_reserve_define_qstring_view_impl(Iter iter,QStringView vw)
+inline constexpr Iter print_reserve_define_qt_scatter_impl(Iter iter,qt_scatter vw)
 {
 	using char_type = std::iter_value_t<Iter>;
 	char_type* ptr(std::to_address(iter));
-	using may_alias_char_type_ptr
-#if __has_cpp_attribute(gnu::may_alias)
-	[[gnu::may_alias]]
-#endif
-	= char16_t const*;
-	may_alias_char_type_ptr newptr{reinterpret_cast<may_alias_char_type_ptr>(vw.data())};
-	std::size_t sz{static_cast<std::size_t>(vw.size())};
-	char_type* ptr_after{utf::utf_code_convert_details<false>(newptr,newptr+sz,ptr)};
+	char_type* ptr_after{utf::utf_code_convert_details<false>(vw.base,vw.base+vw.len,ptr)};
 	return iter+(ptr_after-ptr);
 }
 
@@ -82,9 +88,9 @@ requires (std::same_as<char_type,char>||
 	std::same_as<char_type,char8_t>||
 	std::same_as<char_type,char32_t>)
 inline constexpr std::size_t print_reserve_size(
-	io_reserve_type_t<char_type,manipulators::cdcvt_t<QStringView>>,manipulators::cdcvt_t<QStringView> cdcvt) noexcept
+	io_reserve_type_t<char_type,manipulators::cdcvt_t<details::qt_scatter>>,manipulators::cdcvt_t<details::qt_scatter> cdcvt) noexcept
 {
-	return details::print_reserve_size_qstring_view_impl<char_type>(cdcvt.reference);
+	return details::print_reserve_size_qt_scatter_impl<char_type>(cdcvt.reference);
 }
 
 template<std::random_access_iterator Iter>
@@ -93,17 +99,17 @@ requires (std::same_as<std::iter_value_t<Iter>,char>||
 	std::same_as<std::iter_value_t<Iter>,char8_t>||
 	std::same_as<std::iter_value_t<Iter>,char32_t>)
 inline constexpr Iter print_reserve_define(
-	io_reserve_type_t<std::iter_value_t<Iter>,manipulators::cdcvt_t<QStringView>>,
+	io_reserve_type_t<std::iter_value_t<Iter>,manipulators::cdcvt_t<details::qt_scatter>>,
 	Iter iter,
-	manipulators::cdcvt_t<QStringView> cdcvt)
+	manipulators::cdcvt_t<details::qt_scatter> cdcvt)
 {
-	return details::print_reserve_define_qstring_view_impl(iter,cdcvt.reference);
+	return details::print_reserve_define_qt_scatter_impl(iter,cdcvt.reference);
 }
 
 template<std::integral char_type>
 requires ((std::same_as<char_type,wchar_t>&&sizeof(wchar_t)==sizeof(char16_t))
 	||std::same_as<char_type,char16_t>)
-inline constexpr basic_io_scatter_t<char_type> print_scatter_define(print_scatter_type_t<char_type>,manipulators::cdcvt_t<QStringView> cdcvt)
+inline constexpr basic_io_scatter_t<char_type> print_scatter_define(print_scatter_type_t<char_type>,manipulators::cdcvt_t<details::qt_scatter> cdcvt)
 {
 	using may_alias_char_type_ptr
 #if __has_cpp_attribute(gnu::may_alias)
@@ -111,14 +117,8 @@ inline constexpr basic_io_scatter_t<char_type> print_scatter_define(print_scatte
 #endif
 	= char_type const*;
 	may_alias_char_type_ptr
-	 newptr{reinterpret_cast<may_alias_char_type_ptr>(cdcvt.reference.data())};
-	if constexpr(sizeof(qsizetype)>sizeof(std::size_t))
-	{
-		if(cdcvt.reference.size()>SIZE_MAX)
-			fast_terminate();
-	}
-	std::size_t sz{static_cast<std::size_t>(cdcvt.reference.size())};
-	return {newptr,sz};
+	 newptr{reinterpret_cast<may_alias_char_type_ptr>(cdcvt.reference.base)};
+	return {newptr,cdcvt.reference.len};
 }
 
 }
