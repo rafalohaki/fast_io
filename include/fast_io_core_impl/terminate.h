549fde147567ffc79d393ef047b7d2cce7f6cbfa
expnkx <unlvsur@live.com>
2021-02-22 17:52:27 -0500

    add naive_vector.h to reimplement filesystem internally to avoid std::vector or std::deque because of bad_alloc problems and potentially binary bloat.

--- include/fast_io_core_impl/terminate.h
+++ include/fast_io_core_impl/terminate.h
@@ -6,20 +6,16 @@ namespace fast_io
 
 [[noreturn]] inline void fast_terminate() noexcept
 {
-#ifndef FAST_IO_NOT_TERMINATE
 //https://llvm.org/doxygen/Compiler_8h_source.html
 
 #if defined(_MSC_VER)
 	__debugbreak();
-#else
-#if __has_builtin(__builtin_trap)
+#elif __has_builtin(__builtin_trap)
 	__builtin_trap();
 #else
-	//create a null pointer hardware exception to terminate the program
-	*(reinterpret_cast<char volatile*>(0))=0;
+	std::abort();
 #endif
-#endif
-#endif
+
 }
 
 }
--- include/fast_io_hosted/filesystem/native.h
+++ include/fast_io_hosted/filesystem/native.h
@@ -1,12 +1,12 @@
 #pragma once
 
+#include"naive_vector.h"
 #if _BSD_SOURCE || _SVID_SOURCE || (_POSIX_C_SOURCE >= 200809L || _XOPEN_SOURCE >= 700)
 #include"posix.h"
 #include"posix_at.h"
 #elif _WIN32
 #include"nt.h"
 #endif
-
 
 namespace fast_io
 {
