a600c8c623555cfa9b246dfaf7c317dda302da13
expnkx <unlvsur@live.com>
2021-02-28 18:43:26 -0500

    naive implementation for stvl2

---
+++ include/fast_io_driver/bootloader/stvl2.h
@@ -0,0 +1,219 @@
+#pragma once
+#include<cstdint>
+#include<cstddef>
+#include<type_traits>
+#include<bit>
+
+/*
+https://github.com/stivale/stivale
+
+we just call it stvl2 to avoid naming collision with stivale2
+*/
+
+namespace stvl2
+{
+
+static_assert(sizeof(void*)==4||sizeof(void*)==8);
+static_assert(std::endian::little==std::endian::native);
+struct [[gnu::packed]] stvl2_pointer_padding
+{
+struct stvl2_empty{};
+[[no_unique_address]] std::conditional_t<sizeof(void*)==8,stvl2_empty,std::uint32_t> pad;
+};
+
+enum class stvl2_header_tag:std::uint64_t
+{
+framebuffer_id=0x3ecc1bc43d0f7971,
+fb_mtrr_id=0x4c7bb07731282e00,
+smp_id=0x1ab015085f3273df
+};
+
+enum class stvl2_struct_tag:std::uint64_t
+{
+cmdline_id=0xe5e76a1b4597a781,
+memmap_id=0x2187f79e8612de07,
+framebuffer_id=0x506461d2950408fa,
+fb_mtrr_id=0x6bc1a78ebe871172,
+modules_id=0x4b6fe466aade04ce,
+rsdp_id=0x9e1786930a375e78,
+epoch_id=0x566a7bed888e1407,
+firmware_id=0x359d837855e3858c,
+smp_id=0x34d1d96339647025,
+pxe_server_info=0x29d1e96239247032,
+mmio32_uart=0xb813f9b8dbc78797,
+dtb=0xabb29bd49a2833fa,
+vmap=0xb0ed257db18cb58f
+};
+
+enum class stvl2_mmap_type:std::uint32_t
+{
+usable=1,
+reserved=2,
+acpi_reclaimable=3,
+acpi_nvs=4,
+bad_memory=5,
+bootloader_reclaimable=0x1000,
+kernel_and_modules=0x1001
+};
+
+enum class stvl2_firmware_flags:std::uint64_t
+{
+bios=1<<0
+};
+
+/*
+use stvl2 instead of stivale2 to avoid collision with stivale2.h
+*/
+struct [[gnu::packed]] stvl2_tag
+{
+	std::uint64_t identifier;
+	std::uint64_t next;
+};
+
+struct [[gnu::packed]] stvl2_header
+{
+	[[no_unique_address]] stvl2_pointer_padding entry_point_pointer_pading;
+	void* entry_point;
+	[[no_unique_address]] stvl2_pointer_padding stack_pointer_pading;
+	void* stack;
+	std::uint64_t flags;
+	stvl2_header_tag tags;
+};
+
+struct [[gnu::packed]] stvl2_header_tag_framebuffer:stvl2_tag
+{
+	std::uint16_t framebuffer_width;
+	std::uint16_t framebuffer_height;
+	std::uint16_t framebuffer_bpp;
+};
+
+struct [[gnu::packed]] stvl2_header_tag_smp : stvl2_tag
+{
+	std::uint64_t flags;
+};
+
+struct [[gnu::packed]] stvl2_struct
+{
+	inline static constexpr std::size_t bootloader_brand_size{64};
+	inline static constexpr std::size_t bootloader_version_size{64};
+	char8_t bootloader_brand[bootloader_brand_size];
+	char8_t bootloader_version[bootloader_version_size];
+	stvl2_struct_tag tags;
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_cmdline:stvl2_tag
+{
+	std::uint64_t cmdline;
+};
+
+struct [[gnu::packed]] stvl2_mmap_entry
+{
+	[[no_unique_address]] stvl2_pointer_padding base_pointer_padding;
+	void* base;
+	[[no_unique_address]] stvl2_pointer_padding length_padding;
+	std::size_t length;
+	stvl2_mmap_type type;
+	std::uint32_t unused;
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_memmap:stvl2_tag
+{
+	std::uint64_t entries;
+	[[no_unique_address]] stvl2_mmap_entry memmaps[];
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_framebuffer:stvl2_tag
+{
+	[[no_unique_address]] stvl2_pointer_padding framebuffer_addr_pointer_padding;
+	void* framebuffer_addr;
+	std::uint16_t framebuffer_width;
+	std::uint16_t framebuffer_height;
+	std::uint16_t framebuffer_pitch;
+	std::uint16_t framebuffer_bpp;
+	std::uint8_t  memory_model;
+	std::uint8_t  red_mask_size;
+	std::uint8_t  red_mask_shift;
+	std::uint8_t  green_mask_size;
+	std::uint8_t  green_mask_shift;
+	std::uint8_t  blue_mask_size;
+	std::uint8_t  blue_mask_shift;
+};
+
+struct [[gnu::packed]] stvl2_module
+{
+	[[no_unique_address]] stvl2_pointer_padding module_begin_pointer_padding;
+	void* module_begin;
+	[[no_unique_address]] stvl2_pointer_padding module_end_pointer_padding;
+	void* module_end;
+	static inline constexpr std::size_t module_string_size{128};
+	char8_t module_string[module_string_size];
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_modules:stvl2_tag
+{
+	std::size_t module_count;
+	[[no_unique_address]] stvl2_module modules[];
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_rsdp:stvl2_tag
+{
+	std::uint64_t rdsp;
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_epoch:stvl2_tag
+{
+	std::uint64_t epoch;
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_firmware:stvl2_tag
+{
+	stvl2_firmware_flags tag;
+};
+
+struct [[gnu::packed]] stvl2_smp_info
+{
+	std::uint32_t process_id;
+	std::uint32_t lapic_id;
+	[[no_unique_address]] stvl2_pointer_padding target_stack_pointer_padding;
+	void* target_stack;
+	[[no_unique_address]] stvl2_pointer_padding goto_address_pointer_padding;
+	void* goto_address;
+	[[no_unique_address]] stvl2_pointer_padding extra_argument_pointer_padding;
+	void* extra_argument;
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_smp:stvl2_tag
+{
+	std::uint64_t flags;
+	std::uint32_t bsp_lapic_id;
+	std::uint32_t unused;
+	std::uint64_t cpu_count;
+	[[no_unique_address]] stvl2_smp_info smp_info[];
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_pxe_server_info:stvl2_tag
+{
+	std::uint32_t server_ip;
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_mmio32_uart:stvl2_tag
+{
+	[[no_unique_address]] stvl2_pointer_padding addr_padding;
+	void* addr;
+};
+
+struct [[gnu::packed]] stvl2_struct_tag_dtb:stvl2_tag
+{
+	[[no_unique_address]] stvl2_pointer_padding addr_padding;
+	void* addr;
+	[[no_unique_address]] stvl2_pointer_padding size_padding;
+	std::size_t size;
+};
+
+struct [[gnu::packed]] stvl2_struct_vmap:stvl2_tag
+{
+	[[no_unique_address]] stvl2_pointer_padding addr_padding;
+	void* addr;
+};
+
+}
---
+++ include/fast_io_driver/stvl2.h
@@ -0,0 +1,244 @@
+#pragma once
+#include"../fast_io_concept.h"
+#include"bootloader/stvl2.h"
+
+namespace fast_io
+{
+
+namespace details
+{
+template<std::random_access_iterator Iter>
+inline constexpr Iter print_reserve_define_impl_for_stvl2_tag(Iter iter,stvl2::stvl2_tag tg) noexcept
+{
+	using char_type = std::iter_value_t<Iter>;
+	if constexpr(std::same_as<char_type,char>)
+	{
+		iter=copy_string_literal("identifier: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.identifier);
+		iter=copy_string_literal("\nnext: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.next);
+	}
+	else if constexpr(std::same_as<char_type,wchar_t>)
+	{
+		iter=copy_string_literal(L"identifier: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.identifier);
+		iter=copy_string_literal(L"\nnext: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.next);
+	}
+	else if constexpr(std::same_as<char_type,char16_t>)
+	{
+		iter=copy_string_literal(u"identifier: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.identifier);
+		iter=copy_string_literal(u"\nnext: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.next);
+	}
+	else if constexpr(std::same_as<char_type,char32_t>)
+	{
+		iter=copy_string_literal(U"identifier: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.identifier);
+		iter=copy_string_literal(U"\nnext: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.next);
+	}
+	else
+	{
+		iter=copy_string_literal(u8"identifier: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.identifier);
+		iter=copy_string_literal(u8"\nnext: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.next);
+	}
+}
+
+template<std::random_access_iterator Iter>
+inline constexpr Iter print_reserve_define_impl_for_stvl2_header_tag(Iter iter,stvl2::stvl2_header_tag tg) noexcept
+{
+	using enum stvl2::stvl2_header_tag;
+	using char_type = std::iter_value_t<Iter>;
+	if constexpr(std::same_as<char_type,char>)
+	{
+		switch(tg)
+		{
+		case framebuffer_id:
+			return copy_string_literal("framebuffer_id",iter);
+		case fb_mtrr_id:
+			return copy_string_literal("fb_mtrr_id",iter);
+		case smp_id:
+			return copy_string_literal("smp_id",iter);
+		default:
+			return copy_string_literal("unknown",iter);
+		}
+	}
+	else if constexpr(std::same_as<char_type,wchar_t>)
+	{
+		switch(tg)
+		{
+		case framebuffer_id:
+			return copy_string_literal(L"framebuffer_id",iter);
+		case fb_mtrr_id:
+			return copy_string_literal(L"fb_mtrr_id",iter);
+		case smp_id:
+			return copy_string_literal(L"smp_id",iter);
+		default:
+			return copy_string_literal(L"unknown",iter);
+		}
+	}
+	else if constexpr(std::same_as<char_type,char16_t>)
+	{
+		switch(tg)
+		{
+		case framebuffer_id:
+			return copy_string_literal(u"framebuffer_id",iter);
+		case fb_mtrr_id:
+			return copy_string_literal(u"fb_mtrr_id",iter);
+		case smp_id:
+			return copy_string_literal(u"smp_id",iter);
+		default:
+			return copy_string_literal(u"unknown",iter);
+		}
+	}
+	else if constexpr(std::same_as<char_type,char16_t>)
+	{
+		switch(tg)
+		{
+		case framebuffer_id:
+			return copy_string_literal(U"framebuffer_id",iter);
+		case fb_mtrr_id:
+			return copy_string_literal(U"fb_mtrr_id",iter);
+		case smp_id:
+			return copy_string_literal(U"smp_id",iter);
+		default:
+			return copy_string_literal(U"unknown",iter);
+		}
+	}
+	else
+	{
+		switch(tg)
+		{
+		case framebuffer_id:
+			return copy_string_literal(u8"framebuffer_id",iter);
+		case fb_mtrr_id:
+			return copy_string_literal(u8"fb_mtrr_id",iter);
+		case smp_id:
+			return copy_string_literal(u8"smp_id",iter);
+		default:
+			return copy_string_literal(u8"unknown",iter);
+		}
+	}
+}
+
+template<std::random_access_iterator Iter>
+inline constexpr Iter print_reserve_define_impl_for_stvl2_header(Iter iter,stvl2::stvl2_header tg) noexcept
+{
+	using char_type = std::iter_value_t<Iter>;
+	if constexpr(std::same_as<char_type,char>)
+	{
+		iter=copy_string_literal("entry_point: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.entry_point);
+		iter=copy_string_literal("\nstack: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.stack);
+		iter=copy_string_literal("\nflags: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.flags);
+		iter=copy_string_literal("\ntags: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,stvl2::stvl2_header_tag>,iter,tg.tags);
+	}
+	else if constexpr(std::same_as<char_type,wchar_t>)
+	{
+		iter=copy_string_literal(L"entry_point: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.entry_point);
+		iter=copy_string_literal(L"\nstack: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.stack);
+		iter=copy_string_literal(L"\nflags: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.flags);
+		iter=copy_string_literal(L"\ntags: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,stvl2::stvl2_header_tag>,iter,tg.tags);
+	}
+	else if constexpr(std::same_as<char_type,char16_t>)
+	{
+		iter=copy_string_literal(u"entry_point: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.entry_point);
+		iter=copy_string_literal(u"\nstack: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.stack);
+		iter=copy_string_literal(u"\nflags: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.flags);
+		iter=copy_string_literal(u"\ntags: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,stvl2::stvl2_header_tag>,iter,tg.tags);
+	}
+	else if constexpr(std::same_as<char_type,char32_t>)
+	{
+		iter=copy_string_literal(U"entry_point: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.entry_point);
+		iter=copy_string_literal(U"\nstack: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.stack);
+		iter=copy_string_literal(U"\nflags: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.flags);
+		iter=copy_string_literal(U"\ntags: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,stvl2::stvl2_header_tag>,iter,tg.tags);
+	}
+	else
+	{
+		iter=copy_string_literal(u8"entry_point: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.entry_point);
+		iter=copy_string_literal(u8"\nstack: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,void const*>,iter,tg.stack);
+		iter=copy_string_literal(u8"\nflags: ",iter);
+		iter=print_reserve_define(io_reserve_type<char_type,std::uint64_t>,iter,tg.flags);
+		iter=copy_string_literal(u8"\ntags: ",iter);
+		return print_reserve_define(io_reserve_type<char_type,stvl2::stvl2_header_tag>,iter,tg.tags);
+	}
+}
+
+}
+
+template<std::integral char_type>
+inline constexpr std::size_t print_reserve_size(io_reserve_type_t<char_type,stvl2::stvl2_tag>) noexcept
+{
+	constexpr std::size_t number_size
+	{
+		print_reserve_size(io_reserve_type<char_type,std::uint64_t>)*2
+	};
+	if constexpr(std::same_as<char_type,char>)
+		return number_size+details::string_literal_size("identifier: \nnext: ");
+	else if constexpr(std::same_as<char_type,wchar_t>)
+		return number_size+details::string_literal_size(L"identifier: \nnext: ");
+	else
+		return number_size+details::string_literal_size(u8"identifier: \nnext: ");
+}
+
+template<std::integral char_type>
+inline constexpr std::size_t print_reserve_size(io_reserve_type_t<char_type,stvl2::stvl2_header_tag>) noexcept
+{
+	return 32;
+}
+
+template<std::integral char_type>
+inline constexpr std::size_t print_reserve_size(io_reserve_type_t<char_type,stvl2::stvl2_header>) noexcept
+{
+	constexpr std::size_t tag_size{print_reserve_size(io_reserve_type<char_type,stvl2::stvl2_header_tag>)};
+	if constexpr(std::same_as<char_type,char>)
+		return tag_size+details::string_literal_size("entry_point: \nstack: \nflags: \ntags: ");
+	else if constexpr(std::same_as<char_type,wchar_t>)
+		return tag_size+details::string_literal_size(L"entry_point: \nstack: \nflags: \ntags: ");
+	else
+		return tag_size+details::string_literal_size(u8"entry_point: \nstack: \nflags: \ntags: ");
+}
+
+template<std::random_access_iterator Iter>
+inline constexpr Iter print_reserve_define(io_reserve_type_t<std::iter_value_t<Iter>,stvl2::stvl2_tag>,Iter iter,stvl2::stvl2_tag tg) noexcept
+{
+	return details::print_reserve_define_impl_for_stvl2_tag(iter,tg);
+}
+
+template<std::random_access_iterator Iter>
+inline constexpr Iter print_reserve_define(io_reserve_type_t<std::iter_value_t<Iter>,stvl2::stvl2_header_tag>,Iter iter,stvl2::stvl2_header_tag hd) noexcept
+{
+	return details::print_reserve_define_impl_for_stvl2_header_tag(iter,hd);
+}
+
+template<std::random_access_iterator Iter>
+inline constexpr Iter print_reserve_define(io_reserve_type_t<std::iter_value_t<Iter>,stvl2::stvl2_header>,Iter iter,stvl2::stvl2_header hd) noexcept
+{
+	return details::print_reserve_define_impl_for_stvl2_header(iter,hd);
+}
+
+static_assert(reserve_printable<char8_t,stvl2::stvl2_header>);
+
+}
