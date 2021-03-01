65e4984cdc20939ed00195183492d6808b46c57a
expnkx <unlvsur@live.com>
2021-03-01 03:26:16 -0500

    add zero initialization to all stvl2.h

--- include/fast_io_driver/bootloader/stvl2.h
+++ include/fast_io_driver/bootloader/stvl2.h
@@ -18,7 +18,7 @@ static_assert(std::endian::little==std::endian::native);
 struct [[gnu::packed]] stvl2_pointer_padding
 {
 struct stvl2_empty{};
-[[no_unique_address]] std::conditional_t<sizeof(void*)==8,stvl2_empty,std::uint32_t> pad;
+[[no_unique_address]] std::conditional_t<sizeof(void*)==8,stvl2_empty,std::uint32_t> pad{};
 };
 
 enum class stvl2_header_tag:std::uint64_t
@@ -66,31 +66,31 @@ use stvl2 instead of stivale2 to avoid collision with stivale2.h
 */
 struct [[gnu::packed]] stvl2_tag
 {
-	stvl2_header_tag identifier;
-	stvl2_tag* next;
+	stvl2_header_tag identifier{};
+	stvl2_tag* next{};
 };
 
 struct [[gnu::packed]] stvl2_header
 {
-	[[no_unique_address]] stvl2_pointer_padding entry_point_pointer_padding;
-	void* entry_point;
-	[[no_unique_address]] stvl2_pointer_padding stack_pointer_padding;
-	void* stack;
-	std::uint64_t flags;
-	[[no_unique_address]] stvl2_pointer_padding tags_root_padding;
-	stvl2_tag* tags_root;
+	[[no_unique_address]] stvl2_pointer_padding entry_point_pointer_padding{};
+	void* entry_point{};
+	[[no_unique_address]] stvl2_pointer_padding stack_pointer_padding{};
+	void* stack{};
+	std::uint64_t flags{};
+	[[no_unique_address]] stvl2_pointer_padding tags_root_padding{};
+	stvl2_tag* tags_root{};
 };
 
 struct [[gnu::packed]] stvl2_header_tag_framebuffer : stvl2_tag
 {
-	std::uint16_t framebuffer_width;
-	std::uint16_t framebuffer_height;
-	std::uint16_t framebuffer_bpp;
+	std::uint16_t framebuffer_width{};
+	std::uint16_t framebuffer_height{};
+	std::uint16_t framebuffer_bpp{};
 };
 
 struct [[gnu::packed]] stvl2_header_tag_smp : stvl2_tag
 {
-	std::uint64_t flags;
+	std::uint64_t flags{};
 };
 
 struct [[gnu::packed]] stvl2_struct
@@ -99,53 +99,53 @@ struct [[gnu::packed]] stvl2_struct
 	inline static constexpr std::size_t bootloader_version_size{64};
 	char8_t bootloader_brand[bootloader_brand_size];
 	char8_t bootloader_version[bootloader_version_size];
-	stvl2_tag* tags_root;
+	stvl2_tag* tags_root{};
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_cmdline:stvl2_tag
 {
-	std::uint64_t cmdline;
+	std::uint64_t cmdline{};
 };
 
 struct [[gnu::packed]] stvl2_mmap_entry
 {
 	[[no_unique_address]] stvl2_pointer_padding base_pointer_padding;
-	void* base;
+	void* base{};
 	[[no_unique_address]] stvl2_pointer_padding length_padding;
-	std::size_t length;
-	stvl2_mmap_type type;
-	std::uint32_t unused;
+	std::size_t length{};
+	stvl2_mmap_type type{};
+	std::uint32_t unused{};
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_memmap:stvl2_tag
 {
-	std::uint64_t entries;
+	std::uint64_t entries{};
 	[[no_unique_address]] stvl2_mmap_entry memmaps[];
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_framebuffer:stvl2_tag
 {
 	[[no_unique_address]] stvl2_pointer_padding framebuffer_addr_pointer_padding;
-	void* framebuffer_addr;
-	std::uint16_t framebuffer_width;
-	std::uint16_t framebuffer_height;
-	std::uint16_t framebuffer_pitch;
-	std::uint16_t framebuffer_bpp;
-	std::uint8_t  memory_model;
-	std::uint8_t  red_mask_size;
-	std::uint8_t  red_mask_shift;
-	std::uint8_t  green_mask_size;
-	std::uint8_t  green_mask_shift;
-	std::uint8_t  blue_mask_size;
-	std::uint8_t  blue_mask_shift;
+	void* framebuffer_addr{};
+	std::uint16_t framebuffer_width{};
+	std::uint16_t framebuffer_height{};
+	std::uint16_t framebuffer_pitch{};
+	std::uint16_t framebuffer_bpp{};
+	std::uint8_t  memory_model{};
+	std::uint8_t  red_mask_size{};
+	std::uint8_t  red_mask_shift{};
+	std::uint8_t  green_mask_size{};
+	std::uint8_t  green_mask_shift{};
+	std::uint8_t  blue_mask_size{};
+	std::uint8_t  blue_mask_shift{};
 };
 
 struct [[gnu::packed]] stvl2_module
 {
 	[[no_unique_address]] stvl2_pointer_padding module_begin_pointer_padding;
-	void* module_begin;
+	void* module_begin{};
 	[[no_unique_address]] stvl2_pointer_padding module_end_pointer_padding;
-	void* module_end;
+	void* module_end{};
 	static inline constexpr std::size_t module_string_size{128};
 	char8_t module_string[module_string_size];
 };
@@ -153,69 +153,69 @@ struct [[gnu::packed]] stvl2_module
 struct [[gnu::packed]] stvl2_struct_tag_modules:stvl2_tag
 {
 	[[no_unique_address]] stvl2_pointer_padding module_count_padding;
-	std::size_t module_count;
+	std::size_t module_count{};
 	[[no_unique_address]] stvl2_module modules[];
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_rsdp:stvl2_tag
 {
-	std::uint64_t rdsp;
+	std::uint64_t rdsp{};
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_epoch:stvl2_tag
 {
-	std::uint64_t epoch;
+	std::uint64_t epoch{};
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_firmware:stvl2_tag
 {
-	stvl2_firmware_flags tag;
+	stvl2_firmware_flags tag{};
 };
 
 struct [[gnu::packed]] stvl2_smp_info
 {
-	std::uint32_t process_id;
-	std::uint32_t lapic_id;
+	std::uint32_t process_id{};
+	std::uint32_t lapic_id{};
 	[[no_unique_address]] stvl2_pointer_padding target_stack_pointer_padding;
-	void* target_stack;
+	void* target_stack{};
 	[[no_unique_address]] stvl2_pointer_padding goto_address_pointer_padding;
-	void* goto_address;
+	void* goto_address{};
 	[[no_unique_address]] stvl2_pointer_padding extra_argument_pointer_padding;
-	void* extra_argument;
+	void* extra_argument{};
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_smp:stvl2_tag
 {
-	std::uint64_t flags;
-	std::uint32_t bsp_lapic_id;
-	std::uint32_t unused;
-	std::uint64_t cpu_count;
+	std::uint64_t flags{};
+	std::uint32_t bsp_lapic_id{};
+	std::uint32_t unused{};
+	std::uint64_t cpu_count{};
 	[[no_unique_address]] stvl2_smp_info smp_info[];
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_pxe_server_info:stvl2_tag
 {
-	std::uint32_t server_ip;
+	std::uint32_t server_ip{};
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_mmio32_uart:stvl2_tag
 {
-	[[no_unique_address]] stvl2_pointer_padding addr_padding;
-	void* addr;
+	[[no_unique_address]] stvl2_pointer_padding addr_padding{};
+	void* addr{};
 };
 
 struct [[gnu::packed]] stvl2_struct_tag_dtb:stvl2_tag
 {
-	[[no_unique_address]] stvl2_pointer_padding addr_padding;
-	void* addr;
-	[[no_unique_address]] stvl2_pointer_padding size_padding;
-	std::size_t size;
+	[[no_unique_address]] stvl2_pointer_padding addr_padding{};
+	void* addr{};
+	[[no_unique_address]] stvl2_pointer_padding size_padding{};
+	std::size_t size{};
 };
 
 struct [[gnu::packed]] stvl2_struct_vmap:stvl2_tag
 {
-	[[no_unique_address]] stvl2_pointer_padding addr_padding;
-	void* addr;
+	[[no_unique_address]] stvl2_pointer_padding addr_padding{};
+	void* addr{};
 };
 
 }
