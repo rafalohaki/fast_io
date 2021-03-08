acd175ad1980ed5151eb786e9b77493ded99edec
expnkx <unlvsur@live.com>
2021-03-08 18:27:29 -0500

    add hacks of u8c_io_observer_unlocked for BSD platforms.

--- include/fast_io_legacy_impl/c/bsd.h
+++ include/fast_io_legacy_impl/c/bsd.h
@@ -65,4 +65,82 @@ inline bool underflow(c_io_observer_unlocked cio)
 	return eof;
 }
 
+#if __has_cpp_attribute(gnu::may_alias)
+[[gnu::may_alias]]
+#endif
+inline char8_t* obuffer_begin(u8c_io_observer_unlocked cio) noexcept
+{
+	return reinterpret_cast<char8_t*>(cio.fp->_bf._base);
+}
+
+#if __has_cpp_attribute(gnu::may_alias)
+[[gnu::may_alias]]
+#endif
+inline char8_t* obuffer_curr(u8c_io_observer_unlocked cio) noexcept
+{
+	return reinterpret_cast<char8_t*>(cio.fp->_p);
+}
+
+#if __has_cpp_attribute(gnu::may_alias)
+[[gnu::may_alias]]
+#endif
+inline char8_t* obuffer_end(u8c_io_observer_unlocked cio) noexcept
+{
+	return reinterpret_cast<char8_t*>(cio.fp->_p+cio.fp->_w);
+}
+
+inline void obuffer_set_curr(u8c_io_observer_unlocked cio,
+#if __has_cpp_attribute(gnu::may_alias)
+[[gnu::may_alias]]
+#endif
+char8_t* ptr) noexcept
+{
+	cio.fp->_w-=reinterpret_cast<char unsigned*>(ptr)-cio.fp->_p;
+	cio.fp->_p=reinterpret_cast<char unsigned*>(ptr);
+}
+
+inline void overflow(u8c_io_observer_unlocked cio,char8_t ch)
+{
+	obuffer_set_curr(cio,obuffer_end(cio));
+	if(__sputc(static_cast<int>(static_cast<char unsigned>(ch)),cio.fp)==EOF)[[unlikely]]
+		throw_posix_error();
+}
+
+inline char8_t* ibuffer_begin(u8c_io_observer_unlocked cio) noexcept
+{
+	return reinterpret_cast<char8_t*>(cio.fp->_bf._base);
+}
+
+inline char8_t* ibuffer_curr(u8c_io_observer_unlocked cio) noexcept
+{
+	return reinterpret_cast<char8_t*>(cio.fp->_p);
+}
+
+inline char8_t* ibuffer_end(u8c_io_observer_unlocked cio) noexcept
+{
+	return reinterpret_cast<char8_t*>(cio.fp->_p+cio.fp->_r);
+}
+
+inline void ibuffer_set_curr(u8c_io_observer_unlocked cio,
+#if __has_cpp_attribute(gnu::may_alias)
+[[gnu::may_alias]]
+#endif
+char8_t* ptr) noexcept
+{
+	cio.fp->_r-=reinterpret_cast<char unsigned*>(ptr)-cio.fp->_p;
+	cio.fp->_p=reinterpret_cast<char unsigned*>(ptr);
+}
+
+inline bool underflow(u8c_io_observer_unlocked cio)
+{
+	bool eof{__srget(cio.fp)!=EOF};
+	if(!eof&&ferror_unlocked(cio.fp))
+		throw_posix_error();
+	ibuffer_set_curr(cio,ibuffer_begin(cio));
+	return eof;
+}
+
+static_assert(buffer_io_stream<c_io_observer_unlocked>);
+static_assert(buffer_io_stream<u8c_io_observer_unlocked>);
+
 }
