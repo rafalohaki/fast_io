fdb90998ab7fff406bf41d838603e9ae686ef1d9
expnkx <unlvsur@live.com>
2021-03-09 21:07:50 -0500

    remove some methods in iomutex.h

--- include/fast_io_hosted/iomutex.h
+++ include/fast_io_hosted/iomutex.h
@@ -8,21 +8,13 @@ template<stream T,typename mutex_type = std::mutex>
 class basic_iomutex
 {
 public:
-	T handler;
-	mutex_type mutex;
+	[[no_unique_address]] T handle;
+	[[no_unique_address]] mutex_type mutex;
 	using native_handle_type = T;
 	using char_type = typename native_handle_type::char_type;
 	template<typename ...Args>
 	requires std::constructible_from<T,Args...>
-	constexpr basic_iomutex(Args&& ...args):handler(std::forward<Args>(args)...){}
-	constexpr native_handle_type const& native_handle() const noexcept
-	{
-		return handler;
-	}
-	constexpr native_handle_type& native_handle() noexcept
-	{
-		return handler;
-	}
+	constexpr basic_iomutex(Args&& ...args):handle(std::forward<Args>(args)...){}
 	inline void lock()
 	{
 		mutex.lock();
@@ -35,34 +27,30 @@ public:
 	{
 		mutex.unlock();
 	}
-	inline auto& unlocked_handle() noexcept
-	{
-		return handler;
-	}
 };
 
-template<stream T,typename mutex_type,typename Iter>
+template<input_stream T,typename mutex_type,typename Iter>
 inline constexpr decltype(auto) read(basic_iomutex<T,mutex_type>& mtx,Iter begin,Iter end)
 {
 	std::lock_guard gd{mtx};
-	return read(mtx.unlocked_handle(),begin,end);
+	return read(mtx.handle,begin,end);
 }
-template<stream T,typename mutex_type,typename Iter>
+template<output_stream T,typename mutex_type,typename Iter>
 inline constexpr decltype(auto) write(basic_iomutex<T,mutex_type>& mtx,Iter begin,Iter end)
 {
 	std::lock_guard gd{mtx};
-	return write(mtx.unlocked_handle(),begin,end);
+	return write(mtx.handle,begin,end);
 }
-template<stream T,typename mutex_type,typename Iter>
+template<random_access_stream T,typename mutex_type,typename Iter>
 inline constexpr decltype(auto) seek(basic_iomutex<T,mutex_type>& mtx,Iter begin,Iter end)
 {
 	std::lock_guard gd{mtx};
-	return seek(mtx.unlocked_handle(),begin,end);
+	return seek(mtx.handle,begin,end);
 }
 
 template<typename T,typename mutex_type>
 inline constexpr decltype(auto) redirect_handle(basic_iomutex<T,mutex_type>& t)
 {
-	return redirect_handle(t.native_handle());
+	return redirect_handle(t.handle);
 }
 }
