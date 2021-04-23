ab33bedfafa6d0ecd91fb46e5982ba68cfd0be07
expnkx <unlvsur@live.com>
2021-04-23 15:41:58 -0400

    fix Qt compilation errors

--- include/fast_io_driver/qt.h
+++ include/fast_io_driver/qt.h
@@ -1,18 +1,17 @@
 #pragma once
 
-#if !__has_include("../fast_io.h")
 #include"../fast_io.h"
-#endif
-
 #include<QIODevice>
 #include<QFile>
 #include<QFileDevice>
 #include<QString>
 #include<QStringRef>
+#if defined(QT_VERSION) && defined(QT_VERSION_CHECK)
+#if (QT_VERSION >= QT_VERSION_CHECK(5,6,0))
 #include<QStringView>
-
+#endif
+#endif
 
 #include"qt_impl/qstring.h"
-#include"qt_impl/qerror.h"
 #include"qt_impl/qiodevice.h"
 #include"qt_impl/qfile.h"
--- include/fast_io_driver/qt_impl/qstring.h
+++ include/fast_io_driver/qt_impl/qstring.h
@@ -2,40 +2,30 @@
 
 namespace fast_io
 {
-#if defined(QT_VERSION) && defined(QT_VERSION_CHECK)
-#if (QT_VERSION >= QT_VERSION_CHECK(5,6,0))
-namespace manipulators
-{
-template<
-encoding_scheme src_scheme=encoding_scheme::execution_charset,
-encoding_scheme dst_scheme=encoding_scheme::execution_charset>
-inline constexpr code_cvt_t<src_scheme,dst_scheme,char16_t> code_cvt(QStringView hstr) noexcept
-{
-	using char16_may_alias_ptr
-#if __has_cpp_attribute(gnu::may_alias)
-	[[gnu::may_alias]]
-#endif
-	= char16_t const*;
-	return {{reinterpret_cast<char16_may_alias_ptr>(hstr.c_str()),hstr.size()}};
-}
-}
 
-template<std::integral char_type,
-typename T>
-requires (sizeof(char_type)==sizeof(char16_t)||
-	std::same_as<std::remove_cvref_t<T>,QStringView>>)
-inline constexpr basic_io_scatter_t<char_type> print_scatter_define(print_scatter_type<char_type>,QStringView hstr) noexcept
+class qt_error
 {
-	using char16_may_alias_ptr
-#if __has_cpp_attribute(gnu::may_alias)
-	[[gnu::may_alias]]
-#endif
-	= char_type const*;
-	return {reinterpret_cast<char16_may_alias_ptr>(hstr.c_str()),hstr.size()};
-}
+public:
+	QString qstr;
+	template<typename... Args>
+	requires std::constructible_from<QString,Args...>
+	qt_error(Args&& ...args):qstr(std::forward<Args>(args)...){}
+};
 
+template<typename... Args>
+requires std::constructible_from<QString,Args...>
+inline void throw_qt_error([[maybe_unused]] Args&& ...args)
+{
+#ifdef __cpp_exceptions
+#if defined(_MSC_VER) && (!defined(_HAS_EXCEPTIONS) || _HAS_EXCEPTIONS == 0)
+	fast_terminate();
+#else
+	throw qt_error(std::forward<Args>(args)...);
 #endif
+#else
+	fast_terminate();
 #endif
+}
 
 namespace manipulators
 {
@@ -45,7 +35,14 @@ encoding_scheme dst_scheme=encoding_scheme::execution_charset,
 typename T
 >
 requires (std::same_as<std::remove_cvref_t<T>,QString>||
-	std::same_as<std::remove_cvref_t<T>,QStringRef>)
+	std::same_as<std::remove_cvref_t<T>,QStringRef>||
+	std::same_as<std::remove_cvref_t<T>,qt_error>
+#if defined(QT_VERSION) && defined(QT_VERSION_CHECK)
+#if (QT_VERSION >= QT_VERSION_CHECK(5,6,0))
+	||std::same_as<std::remove_cvref_t<T>,QStringView>
+#endif
+#endif
+	)
 inline constexpr code_cvt_t<src_scheme,dst_scheme,char16_t> code_cvt(T const& hstr) noexcept
 {
 	using char16_may_alias_ptr
@@ -53,7 +50,10 @@ inline constexpr code_cvt_t<src_scheme,dst_scheme,char16_t> code_cvt(T const& hs
 	[[gnu::may_alias]]
 #endif
 	= char16_t const*;
-	return {{reinterpret_cast<char16_may_alias_ptr>(hstr.c_str()),hstr.size()}};
+	if constexpr(std::same_as<std::remove_cvref_t<T>,qt_error>)
+		return {{reinterpret_cast<char16_may_alias_ptr>(hstr.qstr.data()),hstr.qstr.size()}};
+	else
+		return {{reinterpret_cast<char16_may_alias_ptr>(hstr.data()),hstr.size()}};
 }
 }
 
@@ -61,7 +61,14 @@ template<std::integral char_type,
 typename T>
 requires (sizeof(char_type)==sizeof(char16_t)||
 	std::same_as<std::remove_cvref_t<T>,QString>||
-	std::same_as<std::remove_cvref_t<T>,QStringRef>)
+	std::same_as<std::remove_cvref_t<T>,QStringRef>||
+	std::same_as<std::remove_cvref_t<T>,qt_error>
+#if defined(QT_VERSION) && defined(QT_VERSION_CHECK)
+#if (QT_VERSION >= QT_VERSION_CHECK(5,6,0))
+	||std::same_as<std::remove_cvref_t<T>,QStringView>
+#endif
+#endif
+)
 inline constexpr basic_io_scatter_t<char_type> print_scatter_define(print_scatter_type<char_type>,T const& hstr) noexcept
 {
 	using char16_may_alias_ptr
@@ -69,7 +76,10 @@ inline constexpr basic_io_scatter_t<char_type> print_scatter_define(print_scatte
 	[[gnu::may_alias]]
 #endif
 	= char_type const*;
-	return {reinterpret_cast<char16_may_alias_ptr>(hstr.c_str()),hstr.size()};
+	if constexpr(std::same_as<std::remove_cvref_t<T>,qt_error>)
+		return {{reinterpret_cast<char16_may_alias_ptr>(hstr.qstr.data()),hstr.qstr.size()}};
+	else
+		return {reinterpret_cast<char16_may_alias_ptr>(hstr.data()),hstr.size()};
 }
 
 }
