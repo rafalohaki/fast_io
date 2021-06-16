1c2b7f0c5f74b4af79d059993edbdc1317704491
expnkx <unlvsur@live.com>
2021-06-16 09:50:38 -0400

    fix timer.h

--- include/fast_io_driver/timer.h
+++ include/fast_io_driver/timer.h
@@ -5,12 +5,12 @@ namespace fast_io
 
 struct timer
 {
-	std::u8string_view s;
+	::fast_io::freestanding::u8string_view s;
 	unix_timestamp t0;
 	#if __has_cpp_attribute(gnu::cold)
 	[[gnu::cold]]
 	#endif
-	explicit timer(std::u8string_view strvw):s(strvw),t0(posix_clock_gettime(posix_clock_id::monotonic)){}
+	explicit timer(::fast_io::freestanding::u8string_view strvw):s(strvw),t0(posix_clock_gettime(posix_clock_id::monotonic)){}
 	timer(timer const &)=delete;
 	timer& operator=(timer const &)=delete;
 	#if __has_cpp_attribute(gnu::cold)
