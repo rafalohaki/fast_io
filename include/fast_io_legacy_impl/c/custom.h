781d991662765072d2cfec96c48f7441bc096a31
expnkx <unlvsur@live.com>
2021-06-19 13:07:41 -0400

    fix some issues with custom.h never tested it

--- include/fast_io_legacy_impl/c/custom.h
+++ include/fast_io_legacy_impl/c/custom.h
@@ -3,103 +3,103 @@
 namespace fast_io
 {
 
-template<c_family family,std::integral char_type>
+template<std::integral char_type>
 requires requires(FILE* fp)
 {
 	{::std::posix::__libc_stdio_fp_ibuffer_begin<char_type>(fp)}->char_type*;
 }
-inline char_type* ibuffer_begin(basic_c_io_observer_unlocked<family,char_type> ciob) noexcept
+inline char_type* ibuffer_begin(basic_c_io_observer_unlocked<char_type> ciob) noexcept
 {
 	return ::std::posix::__libc_stdio_fp_ibuffer_begin<char_type>(ciob.fp);
 }
 
-template<c_family family,std::integral char_type>
+template<std::integral char_type>
 requires requires(FILE* fp)
 {
 	{::std::posix::__libc_stdio_fp_ibuffer_curr<char_type>(fp)}->char_type*;
 }
-inline char_type* ibuffer_curr(basic_c_io_observer_unlocked<family,char_type> ciob) noexcept
+inline char_type* ibuffer_curr(basic_c_io_observer_unlocked<char_type> ciob) noexcept
 {
 	return ::std::posix::__libc_stdio_fp_ibuffer_curr<char_type>(ciob.fp);
 }
 
-template<c_family family,std::integral char_type>
+template<std::integral char_type>
 requires requires(FILE* fp)
 {
 	{::std::posix::__libc_stdio_fp_ibuffer_end<char_type>(fp)}->char_type*;
 }
-inline char_type* ibuffer_end(c_io_observer_unlocked ciob) noexcept
+inline char_type* ibuffer_end(basic_c_io_observer_unlocked<char_type> ciob) noexcept
 {
 	return ::std::posix::__libc_stdio_fp_ibuffer_end<char_type>(ciob.fp);
 }
 
-template<c_family family,std::integral char_type>
+template<std::integral char_type>
 requires requires(FILE* fp,char_type* ptr)
 {
-	{::std::posix::__libc_stdio_fp_ibuffer_set_curr<char_type>(fp)}->char_type*;
+	{::std::posix::__libc_stdio_fp_ibuffer_set_curr<char_type>(fp,ptr)};
 }
-inline char_type* ibuffer_set_curr(c_io_observer_unlocked ciob,char_type* ptr) noexcept
+inline void ibuffer_set_curr(basic_c_io_observer_unlocked<char_type> ciob,char_type* ptr) noexcept
 {
-	return ::std::posix::__libc_stdio_fp_ibuffer_set_curr<char_type>(ciob.fp,ptr);
+	::std::posix::__libc_stdio_fp_ibuffer_set_curr<char_type>(ciob.fp,ptr);
 }
 
 template<c_family family,std::integral char_type>
-inline bool ibuffer_underflow(c_io_observer_unlocked ciob)
-requires requires(FILE* fp,char_type* ptr)
+inline bool ibuffer_underflow(basic_c_io_observer_unlocked<char_type> ciob)
+requires requires(FILE* fp)
 {
 	{::std::posix::__libc_stdio_fp_ibuffer_underflow<char_type>(fp)}->bool;
 }
 {
-	return ::std::posix::__libc_stdio_fp_ibuffer_underflow<char_type>(ciob.fp,ptr);
+	return ::std::posix::__libc_stdio_fp_ibuffer_underflow<char_type>(ciob.fp);
 }
 
 
-template<c_family family,std::integral char_type>
+template<std::integral char_type>
 requires requires(FILE* fp)
 {
 	{::std::posix::__libc_stdio_fp_obuffer_begin<char_type>(fp)}->char_type*;
 }
-inline char_type* obuffer_begin(basic_c_io_observer_unlocked<family,char_type> ciob) noexcept
+inline char_type* obuffer_begin(basic_c_io_observer_unlocked<char_type> ciob) noexcept
 {
 	return ::std::posix::__libc_stdio_fp_obuffer_begin<char_type>(ciob.fp);
 }
 
-template<c_family family,std::integral char_type>
+template<std::integral char_type>
 requires requires(FILE* fp)
 {
 	{::std::posix::__libc_stdio_fp_obuffer_curr<char_type>(fp)}->char_type*;
 }
-inline char_type* obuffer_curr(basic_c_io_observer_unlocked<family,char_type> ciob) noexcept
+inline char_type* obuffer_curr(basic_c_io_observer_unlocked<char_type> ciob) noexcept
 {
 	return ::std::posix::__libc_stdio_fp_obuffer_curr<char_type>(ciob.fp);
 }
 
-template<c_family family,std::integral char_type>
+template<std::integral char_type>
 requires requires(FILE* fp)
 {
 	{::std::posix::__libc_stdio_fp_obuffer_end<char_type>(fp)}->char_type*;
 }
-inline char_type* obuffer_end(c_io_observer_unlocked ciob) noexcept
+inline char_type* obuffer_end(basic_c_io_observer_unlocked<char_type> ciob) noexcept
 {
 	return ::std::posix::__libc_stdio_fp_obuffer_end<char_type>(ciob.fp);
 }
 
-template<c_family family,std::integral char_type>
+template<std::integral char_type>
 requires requires(FILE* fp,char_type* ptr)
 {
 	{::std::posix::__libc_stdio_fp_obuffer_set_curr<char_type>(fp,ptr)}->char_type*;
 }
-inline char_type* obuffer_set_curr(c_io_observer_unlocked ciob,char_type* ptr) noexcept
+inline char_type* obuffer_set_curr(basic_c_io_observer_unlocked<char_type> ciob,char_type* ptr) noexcept
 {
 	return ::std::posix::__libc_stdio_fp_obuffer_set_curr<char_type>(ciob.fp,ptr);
 }
 
-template<c_family family,std::integral char_type>
+template<std::integral char_type>
 requires requires(FILE* fp,char_type ch)
 {
-	{::std::posix::__libc_stdio_fp_obuffer_overflow<char_type>(fp,ch)}->bool;
+	::std::posix::__libc_stdio_fp_obuffer_overflow<char_type>(fp,ch);
 }
-inline bool obuffer_overflow(c_io_observer_unlocked ciob,char_type ch)
+inline void obuffer_overflow(basic_c_io_observer_unlocked<char_type> ciob,char_type ch)
 {
 	return ::std::posix::__libc_stdio_fp_obuffer_overflow<char_type>(ciob.fp,ch);
 }
