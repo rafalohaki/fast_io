4db55c08c572a4618e48c9a535e92a6a579e66dd
expnkx <unlvsur@live.com>
2021-07-17 03:11:18 -0400

    iostream

--- benchmark/0001.refterm/iostream.cc
+++ benchmark/0001.refterm/iostream.cc
@@ -1,10 +1,13 @@
-#include<iostream>
+inline void test()
+{
+	for(std::size_t i{};i!=1000000;++i)
+		std::cout<<i<<'\n';
+}
 
 int main()
 {
 	fast_io::u8obuf_file timer_obf(u"iostream.txt");
 	auto t0{posix_clock_gettime(fast_io::posix_clock_id::monotonic)};
-	for(std::size_t i{};i!=1000000;++i)
-		std::cout<<i<<'\n';
+	test();
 	print(timer_obf,posix_clock_gettime(fast_io::posix_clock_id::monotonic)-t0,u8"s\n");
 }
