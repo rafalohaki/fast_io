f4e475b90504d8c6ddf08df8e0b82700d219e2c4
expnkx <unlvsur@live.com>
2021-08-05 21:04:53 -0400

    c_file_unlocked requires open_mode

--- fuzzing/0000.write/c_file_unlocked.cc
+++ fuzzing/0000.write/c_file_unlocked.cc
@@ -1,6 +1,6 @@
 #include<fast_io.h>
 
-fast_io::c_file_unlocked obf("/dev/null");
+fast_io::c_file_unlocked obf("/dev/null",fast_io::open_mode::out);
 
 extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const* ptr,std::size_t n) noexcept
 {
