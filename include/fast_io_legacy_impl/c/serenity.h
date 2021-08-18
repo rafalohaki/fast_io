313e3ec7956539583c26f79838567e0fd5108304
expnkx <unlvsur@live.com>
2021-08-18 00:31:36 -0400

    fix compilation errors with serenity's FILE*

--- include/fast_io_legacy_impl/c/serenity.h
+++ include/fast_io_legacy_impl/c/serenity.h
@@ -46,47 +46,52 @@ inline char* serenity_obuffer_begin(serenity_file_model*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-ptr) noexcept
+fp) noexcept
 {
-	return reinterpret_cast<char*>(ptr->m_buffer.m_data);
+	return reinterpret_cast<char*>(fp->m_buffer.m_data);
 }
 
 inline char* serenity_obuffer_curr(serenity_file_model*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-ptr) noexcept
+fp) noexcept
 {
-	return reinterpret_cast<char*>(ptr->m_buffer.m_data)+ptr->m_buffer.m_begin;
+	return reinterpret_cast<char*>(fp->m_buffer.m_data)+fp->m_buffer.m_begin;
 }
 
 inline char* serenity_obuffer_end(serenity_file_model*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-ptr) noexcept
+fp) noexcept
 {
-	return reinterpret_cast<char*>(ptr->m_buffer.m_data)+ptr->m_buffer.m_end;
+	return reinterpret_cast<char*>(fp->m_buffer.m_data)+fp->m_buffer.m_end;
 }
 
 inline void serenity_obuffer_set_curr(serenity_file_model*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-ptr,char * ptr) noexcept
+fp,char * ptr) noexcept
 {
-	ptr->m_buffer.m_begin=static_cast<std::size_t>(ptr-ptr->m_buffer.m_data);
+	fp->m_buffer.m_begin=static_cast<std::size_t>(ptr-reinterpret_cast<char*>(fp->m_buffer.m_data));
 }
 
 inline void serenity_obuffer_overflow(serenity_file_model*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-ptr,char ch)
+fp,char ch)
 {
-	int ret{noexcept_call(::fputc,ch)};
+	using file_ptr_may_alias
+#if __has_cpp_attribute(gnu::may_alias)
+[[gnu::may_alias]]
+#endif
+	= FILE*;
+	int ret{noexcept_call(::fputc,ch,reinterpret_cast<file_ptr_may_alias>(fp))};
 	if(ret==EOF)
-		throw_posix_error());
+		throw_posix_error();
 }
 
 
@@ -94,36 +99,36 @@ inline char* serenity_ibuffer_begin(serenity_file_model*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-ptr) noexcept
+fp) noexcept
 {
-	return reinterpret_cast<char*>(ptr->m_buffer.m_data);
+	return reinterpret_cast<char*>(fp->m_buffer.m_data);
 }
 
 inline char* serenity_ibuffer_curr(serenity_file_model*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-ptr) noexcept
+fp) noexcept
 {
-	return reinterpret_cast<char*>(ptr->m_buffer.m_data)+ptr->m_buffer.m_begin;
+	return reinterpret_cast<char*>(fp->m_buffer.m_data)+fp->m_buffer.m_begin;
 }
 
-inline char* serenity_obuffer_end(serenity_file_model*
+inline char* serenity_ibuffer_end(serenity_file_model*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-ptr) noexcept
+fp) noexcept
 {
-	return reinterpret_cast<char*>(ptr->m_buffer.m_data)+ptr->m_buffer.m_end;
+	return reinterpret_cast<char*>(fp->m_buffer.m_data)+fp->m_buffer.m_end;
 }
 
-inline void serenity_obuffer_set_curr(serenity_file_model*
+inline void serenity_ibuffer_set_curr(serenity_file_model*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-ptr,char* ptr) noexcept
+fp,char* ptr) noexcept
 {
-	ptr->m_buffer.m_begin=static_cast<std::size_t>(ptr-ptr->m_buffer.m_data);
+	fp->m_buffer.m_begin=static_cast<std::size_t>(ptr-reinterpret_cast<char*>(fp->m_buffer.m_data));
 }
 
 inline bool serenity_ibuffer_underflow(serenity_file_model*
@@ -132,7 +137,7 @@ inline bool serenity_ibuffer_underflow(serenity_file_model*
 #endif
 fp)
 {
-	ptr->m_buffer.m_begin=ptr->m_buffer.m_end;
+	fp->m_buffer.m_begin=fp->m_buffer.m_end;
 	using file_ptr_may_alias
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
@@ -141,11 +146,11 @@ fp)
 	int c{noexcept_call(fgetc,reinterpret_cast<file_ptr_may_alias>(fp))};
 	if(c==EOF)
 	{
-		if(ptr->m_buffer->m_error)
-			throw_posix_erro();
+		if(fp->m_error)
+			throw_posix_error();
 		return false;
 	}
-	ptr->m_buffer.m_begin=0;
+	fp->m_buffer.m_begin=0;
 	return true;
 }
 
@@ -158,7 +163,7 @@ requires (sizeof(char_type)==1)
 #endif
 inline char_type* ibuffer_begin(basic_c_io_observer_unlocked<char_type> bciob) noexcept
 {
-	return details::serenity_ibuffer_begin(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp));
+	return ::fast_io::details::serenity_ibuffer_begin(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp));
 }
 
 template<std::integral char_type>
@@ -168,7 +173,7 @@ requires (sizeof(char_type)==1)
 #endif
 inline char_type* ibuffer_curr(basic_c_io_observer_unlocked<char_type> bciob) noexcept
 {
-	return details::serenity_ibuffer_curr(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp));
+	return ::fast_io::details::serenity_ibuffer_curr(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp));
 }
 
 template<std::integral char_type>
@@ -178,7 +183,7 @@ requires (sizeof(char_type)==1)
 #endif
 inline char_type* ibuffer_end(basic_c_io_observer_unlocked<char_type> bciob) noexcept
 {
-	return details::serenity_ibuffer_end(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp));
+	return ::fast_io::details::serenity_ibuffer_end(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp));
 }
 
 template<std::integral char_type>
@@ -186,20 +191,20 @@ requires (sizeof(char_type)==1)
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-inline char_type* ibuffer_set_curr(basic_c_io_observer_unlocked<char_type> bciob,char_type*
+inline void ibuffer_set_curr(basic_c_io_observer_unlocked<char_type> bciob,char_type*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-char_type* ptr
+ptr
 ) noexcept
 {
-	return details::serenity_ibuffer_curr(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp),ptr);
+	::fast_io::details::serenity_ibuffer_set_curr(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp),reinterpret_cast<char*>(ptr));
 }
 
 template<std::integral char_type>
 inline bool ibuffer_underflow(basic_c_io_observer_unlocked<char_type> bciob) noexcept
 {
-	return details::serenity_ibuffer_underflow(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp));
+	return ::fast_io::details::serenity_ibuffer_underflow(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp));
 }
 
 
@@ -211,7 +216,7 @@ requires (sizeof(char_type)==1)
 #endif
 inline char_type* obuffer_begin(basic_c_io_observer_unlocked<char_type> bciob) noexcept
 {
-	return details::serenity_obuffer_begin(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp));
+	return ::fast_io::details::serenity_obuffer_begin(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp));
 }
 
 template<std::integral char_type>
@@ -221,7 +226,7 @@ requires (sizeof(char_type)==1)
 #endif
 inline char_type* obuffer_curr(basic_c_io_observer_unlocked<char_type> bciob) noexcept
 {
-	return details::serenity_obuffer_curr(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp));
+	return ::fast_io::details::serenity_obuffer_curr(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp));
 }
 
 template<std::integral char_type>
@@ -231,7 +236,7 @@ requires (sizeof(char_type)==1)
 #endif
 inline char_type* obuffer_end(basic_c_io_observer_unlocked<char_type> bciob) noexcept
 {
-	return details::serenity_obuffer_end(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp));
+	return ::fast_io::details::serenity_obuffer_end(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp));
 }
 
 template<std::integral char_type>
@@ -239,21 +244,21 @@ requires (sizeof(char_type)==1)
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-inline char_type* obuffer_set_curr(basic_c_io_observer_unlocked<char_type> bciob,char_type*
+inline void obuffer_set_curr(basic_c_io_observer_unlocked<char_type> bciob,char_type*
 #if __has_cpp_attribute(gnu::may_alias)
 [[gnu::may_alias]]
 #endif
-char_type* ptr
+ptr
 ) noexcept
 {
-	return details::serenity_obuffer_curr(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp),ptr);
+	::fast_io::details::serenity_obuffer_set_curr(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp),reinterpret_cast<char*>(ptr));
 }
 
 template<std::integral char_type>
 requires (sizeof(char_type)==1)
 inline void obuffer_overflow(basic_c_io_observer_unlocked<char_type> bciob,char_type ch) noexcept
 {
-	return details::serenity_obuffer_overflow(reinterpret_cast<::fast_io::details::serenity_file_model*>(ciob.fp),ch);
+	return ::fast_io::details::serenity_obuffer_overflow(reinterpret_cast<::fast_io::details::serenity_file_model*>(bciob.fp),ch);
 }
 
 }
