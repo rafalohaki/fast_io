be82b30461663aacde1c4bd7a6065b16279a67ff
expnkx <unlvsur@live.com>
2021-09-08 11:37:09 -0400

    gcc_symlink_install.cc needs sorting and filtering py file

--- examples/0009.filesystem/gcc_symlink_install.cc
+++ examples/0009.filesystem/gcc_symlink_install.cc
@@ -4,6 +4,7 @@
 #include<string_view>
 #include<vector>
 #include<algorithm>
+#include<ranges>
 
 int main(int argc,char const** argv)
 {
@@ -14,17 +15,23 @@ int main(int argc,char const** argv)
 	}
 	std::vector<std::u8string> names;
 	fast_io::dir_file df(argv[1]);
+	using namespace std::string_view_literals;
 	for(auto const& ent:current(at(df)))
 	{
 		auto ftype{type(ent)};
 		if(ftype==fast_io::file_type::regular||ftype==fast_io::file_type::symlink)
 		{
+			auto exts{extension(ent)};
+			std::u8string_view view(exts.cbegin(),exts.cend());
+			if(view==u8".py"sv)	//excluse .py like libstdc++.so.6.0.29-gdb.py
+				continue;
 			auto name{filename(ent)};
 			names.emplace_back(name.cbegin(),name.cend());
 		}
 	}
 	if(names.empty())
 		return 0;
+	std::ranges::sort(names);
 	std::size_t const sz{names.size()-1};
 	for(std::size_t i{};i!=sz;++i)
 	{
@@ -32,6 +39,7 @@ int main(int argc,char const** argv)
 			continue;
 		if(!std::equal(names[i].cbegin(),names[i].cend(),names[i+1].cbegin()))
 			continue;
+		perrln(fast_io::u8out(),names[i],u8" -> ",names[i+1]);
 		posix_unlinkat(at(df),names[i]);
 		posix_symlinkat(names[i+1],at(df),names[i]);
 	}
