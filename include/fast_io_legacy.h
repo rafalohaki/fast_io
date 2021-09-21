52b01a22b80cb1329739a03638ab25aba4f4df9e
expnkx <unlvsur@live.com>
2021-09-21 02:43:35 -0400

    outdated comment fix. fast_io_legacy.h only deals with C++ fstream interface. stdio is already included by fast_io.h

--- include/fast_io_legacy.h
+++ include/fast_io_legacy.h
@@ -6,7 +6,7 @@
 #if defined(__GNUC__) && __GNUC__>=11 && __cplusplus<202002L
 #error "fast_io requires at least C++20 standard compiler."
 #else
-//fast_io_legacy.h deals with legacy C <stdio.h> and C++ <iostream>/<fstream>/<sstream> interface
+//fast_io_legacy.h deals with legacy C++ <iostream>/<fstream>/<sstream> interface
 #if __STDC_HOSTED__==1 && (!defined(_GLIBCXX_HOSTED) || _GLIBCXX_HOSTED==1)
 #include<streambuf>
 #include<fstream>
