69f4466ee09389262d81591935d9150ae54d1570
expnkx <unlvsur@live.com>
2021-10-13 06:28:51 -0400

    http header parse rewrite

---
+++ include/fast_io_unit/http_header.h
@@ -0,0 +1,307 @@
+#pragma once
+
+namespace fast_io
+{
+
+template<std::integral ch_type>
+struct basic_http_header_buffer
+{
+	using char_type = ch_type;
+	std::size_t header_length{};
+	std::size_t http_version_end_location{};
+	std::size_t http_status_code_start_location{};
+	std::size_t http_status_code_end_location{};
+	std::size_t http_status_reason_start_location{};
+	std::size_t http_status_reason_end_location{};
+	char_type buffer[4096];
+	inline constexpr ::std::basic_string_view<char_type> http_version() const noexcept
+	{
+		return ::std::basic_string_view<char_type>(buffer,buffer+http_version_end_location);
+	}
+	inline constexpr ::std::basic_string_view<char_type> http_status_code() const noexcept
+	{
+		return ::std::basic_string_view<char_type>(buffer+http_status_code_start_location,buffer+http_status_code_end_location);
+	}
+	inline constexpr ::std::basic_string_view<char_type> http_status_reason() const noexcept
+	{
+		return ::std::basic_string_view<char_type>(buffer+http_status_reason_start_location,buffer+http_status_reason_end_location);
+	}
+};
+
+template<std::integral char_type>
+inline constexpr auto scan_context_type(io_reserve_type_t<char_type,::fast_io::parameter<::fast_io::basic_http_header_buffer<char_type>&>>) noexcept
+{
+	struct ctx
+	{
+		std::size_t state{};
+	};
+	return io_type_t<ctx>{};
+}
+
+template<std::integral ch_type>
+inline constexpr basic_io_scatter_t<ch_type> print_alias_define(io_alias_t,basic_http_header_buffer<ch_type> const& b) noexcept
+{
+	return {b.buffer,b.header_length};
+}
+
+namespace details
+{
+template<std::integral ch_type,::fast_io::freestanding::random_access_iterator Iter>
+inline bool try_copy_into_buffer(basic_http_header_buffer<ch_type>& b,Iter first,Iter last) noexcept
+{
+	std::size_t diff{static_cast<std::size_t>(last-first)};
+	std::size_t remain_space{static_cast<std::size_t>(4096u-b.header_length)};
+	if(remain_space<diff)
+		return false;
+	non_overlapped_copy_n(first,diff,b.buffer+b.header_length);
+	b.header_length+=diff;
+	return true;
+}
+
+template<std::integral ch_type>
+inline constexpr parse_code determine_http_header_location(basic_http_header_buffer<ch_type>& b) noexcept
+{
+	auto i{b.buffer},e{b.buffer+b.header_length};
+	if(i!=e&&*i==char_literal_v<u8' ',ch_type>)
+		return parse_code::invalid;
+	{
+	auto j{i};
+	for(;i!=e&&*i!=char_literal_v<u8' ',ch_type>;++i);
+	auto diff{i-j};
+	if(i==e||diff<=8||diff>=10)
+	{
+		return parse_code::invalid;
+	}
+	if(*j!=char_literal_v<u8'H',ch_type>||j[1]!=char_literal_v<u8'T',ch_type>||j[2]!=char_literal_v<u8'T',ch_type>||j[3]!=char_literal_v<u8'P',ch_type>||j[4]!=char_literal_v<u8'/',ch_type>)
+		return parse_code::invalid;
+	b.http_version_end_location=static_cast<std::size_t>(i-b.buffer);
+	}
+	++i;
+	for(;i!=e&&*i==char_literal_v<u8' ',ch_type>;++i);
+	if(i==e)
+		return parse_code::invalid;
+	b.http_status_code_start_location=static_cast<std::size_t>(i-b.buffer);
+	{
+	auto j{i};
+	for(;i!=e&&*i!=char_literal_v<u8' ',ch_type>;++i);
+	if(i==e||i-j!=3)
+		return parse_code::invalid;
+	if(!::fast_io::char_category::is_c_digit(*j)||!::fast_io::char_category::is_c_digit(j[1])||!::fast_io::char_category::is_c_digit(j[2]))
+		return parse_code::invalid;
+	b.http_status_code_end_location=static_cast<std::size_t>(i-b.buffer);
+	}
+	for(;i!=e&&*i==char_literal_v<u8' ',ch_type>;++i);
+	if(i==e)
+		return parse_code::invalid;
+	b.http_status_reason_start_location=static_cast<std::size_t>(i-b.buffer);
+	for(;i!=e&&*i!=char_literal_v<u8'\r',ch_type>;++i);
+	if(i==e)
+		return parse_code::invalid;
+	b.http_status_reason_end_location=static_cast<std::size_t>(i-b.buffer);
+	return parse_code::ok;
+}
+
+}
+
+template<::fast_io::freestanding::random_access_iterator Iter,typename T>
+inline constexpr parse_result<Iter> scan_context_define2(io_reserve_type_t<::fast_io::freestanding::iter_value_t<Iter>,::fast_io::parameter<basic_http_header_buffer<::fast_io::freestanding::iter_value_t<Iter>>&>>,T& statetp,Iter first1,Iter last,::fast_io::parameter<basic_http_header_buffer<::fast_io::freestanding::iter_value_t<Iter>>&> t) noexcept
+{
+	auto& state{statetp.state};
+	using ch_type = ::fast_io::freestanding::iter_value_t<Iter>;
+	auto first{first1};
+	if(first==last)
+	{
+		return {first,parse_code::partial};
+	}
+	if(state!=0)
+	{
+		switch(state)
+		{
+		case 1:
+		{
+			if(*first!=char_literal_v<u8'\n',ch_type>)
+				break;
+			++first;
+			if(first==last)
+			{
+				state=2u;
+				if(!::fast_io::details::try_copy_into_buffer(t.reference,first1,last))
+					return {first,parse_code::invalid};
+				return {first,parse_code::partial};
+			}
+			[[fallthrough]];
+		}
+		case 2:
+		{
+			if(*first!=char_literal_v<u8'\r',ch_type>)
+				break;
+			++first;
+			if(first==last)
+			{
+				state=3u;
+				if(!::fast_io::details::try_copy_into_buffer(t.reference,first1,last))
+					return {first,parse_code::invalid};
+				return {first,parse_code::partial};
+			}
+			[[fallthrough]];
+		}
+		case 3:
+		{
+			if(*first!=char_literal_v<u8'\n',ch_type>)
+				break;
+			++first;
+			if(!::fast_io::details::try_copy_into_buffer(t.reference,first1,first))
+				return {first,parse_code::invalid};
+			return {first,::fast_io::details::determine_http_header_location(t.reference)};
+		}
+		}
+	}
+	state=0;
+	for(;;)
+	{
+		for(;first!=last&&*first!=char_literal_v<u8'\r',ch_type>;++first);
+		if(first==last)
+		{
+			if(!::fast_io::details::try_copy_into_buffer(t.reference,first1,first))
+				return {first,parse_code::invalid};
+			state=0u;
+			return {first,parse_code::partial};
+		}
+		++first;
+		if(first==last)
+		{
+			if(!::fast_io::details::try_copy_into_buffer(t.reference,first1,first))
+				return {first,parse_code::invalid};
+			state=1u;
+			return {first,parse_code::partial};
+		}
+		if(*first!=char_literal_v<u8'\n',ch_type>)
+		{
+			return {first,parse_code::invalid};
+		}
+		++first;
+		if(first==last)
+		{
+			if(!::fast_io::details::try_copy_into_buffer(t.reference,first1,first))
+				return {first,parse_code::invalid};
+			state=2u;
+			return {first,parse_code::partial};
+		}
+		if(*first!=char_literal_v<u8'\r',ch_type>)
+		{
+			continue;
+		}
+		++first;
+		if(first==last)
+		{
+			if(!::fast_io::details::try_copy_into_buffer(t.reference,first1,first))
+				return {first,parse_code::invalid};
+			state=3u;
+			return {first,parse_code::partial};
+		}
+		if(*first!=char_literal_v<u8'\n',ch_type>)
+		{
+			return {first,parse_code::invalid};
+		}
+		++first;
+		if(!::fast_io::details::try_copy_into_buffer(t.reference,first1,first))
+			return {first,parse_code::invalid};
+		return {first,::fast_io::details::determine_http_header_location(t.reference)};
+	}
+}
+
+
+template<std::integral char_type,typename T>
+inline constexpr parse_code scan_context_eof_define(io_reserve_type_t<char_type,::fast_io::parameter<basic_http_header_buffer<char_type>&>>,T,::fast_io::parameter<basic_http_header_buffer<char_type>&>) noexcept
+{
+	return parse_code::invalid;
+}
+
+using http_header_buffer = basic_http_header_buffer<char>;
+using u8http_header_buffer = basic_http_header_buffer<char8_t>;
+using u16http_header_buffer = basic_http_header_buffer<char16_t>;
+using u32http_header_buffer = basic_http_header_buffer<char32_t>;
+using whttp_header_buffer = basic_http_header_buffer<wchar_t>;
+
+template<std::integral char_type>
+struct basic_http_line_generator
+{
+	char_type const* current{};
+	char_type const* last{};
+	char_type const* key_end{};
+	char_type const* value_start{};
+	char_type const* value_end{};
+};
+
+template<std::integral char_type>
+struct basic_http_line
+{
+	::std::basic_string_view<char_type> key,value;
+};
+
+template<std::integral char_type>
+inline constexpr basic_http_line_generator<char_type> line_generator(basic_http_header_buffer<char_type>& b) noexcept
+{
+	std::size_t end_loc{b.http_status_reason_end_location};
+	char_type const* start{b.buffer+end_loc};
+	return {start,b.buffer+b.header_length,start,start,start};
+}
+
+template<std::integral char_type>
+inline constexpr basic_http_line_generator<char_type>& operator++(basic_http_line_generator<char_type>& b) noexcept
+{
+	auto const last{b.last};
+	b.current=b.value_end;
+	if(last-b.current<=4)
+	{
+		b.value_end=b.value_start=b.key_end=b.current=last;
+		return b;
+	}
+	for(;b.current!=last&&*b.current!=char_literal_v<u8'\n',char_type>;++b.current);
+	if(b.current==last)
+	{
+		b.value_end=b.value_start=b.key_end=last;
+		return b;
+	}
+	for(++b.current;b.current!=last&&*b.current==char_literal_v<u8' ',char_type>;++b.current);
+	auto curr{b.current};
+	for(;curr!=last&&*curr!=char_literal_v<u8' ',char_type>;++curr);
+	b.key_end=curr;
+	for(;curr!=last&&*curr==char_literal_v<u8' ',char_type>;++curr);
+	b.value_start=curr;
+	for(;curr!=last&&*curr!=char_literal_v<u8'\r',char_type>;++curr);
+	b.value_end=curr;
+	return b;
+}
+
+template<std::integral char_type>
+inline constexpr basic_http_line_generator<char_type>& begin(basic_http_line_generator<char_type>& b) noexcept
+{
+	return ++b;
+}
+
+template<std::integral char_type>
+inline constexpr ::fast_io::freestanding::default_sentinel_t end(basic_http_line_generator<char_type>&) noexcept
+{
+	return {};
+}
+
+template<std::integral char_type>
+inline constexpr bool operator==(basic_http_line_generator<char_type>& b,::fast_io::freestanding::default_sentinel_t)
+{
+	return b.current==b.last;
+}
+
+template<std::integral char_type>
+inline constexpr bool operator!=(basic_http_line_generator<char_type>& b,::fast_io::freestanding::default_sentinel_t)
+{
+	return b.current!=b.last;
+}
+
+template<std::integral char_type>
+inline constexpr basic_http_line<char_type> operator*(basic_http_line_generator<char_type>& b) noexcept
+{
+	return {::std::basic_string_view<char_type>(b.current,static_cast<std::size_t>(b.key_end-b.current)),::std::basic_string_view<char_type>(b.value_start,static_cast<std::size_t>(b.value_end-b.value_start))};
+}
+
+}
