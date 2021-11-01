4d7cb487de56ec7a39321a497d870b23eece5f85
expnkx <unlvsur@live.com>
2021-10-31 20:06:32 -0400

    fix ranges include on libcxx

--- include/fast_io_core_impl/freestanding/impl.h
+++ include/fast_io_core_impl/freestanding/impl.h
@@ -13,3 +13,11 @@ no std::move, std::forward, std::addressof. WTF???
 #include"cstr_len.h"
 #include"string_view.h"
 #include"algorithm.h"
+
+#if _LIBCPP_VERSION
+#if __has_include(<__ranges/concepts.h>)
+#include<__ranges/concepts.h>
+#else
+#include<ranges>
+#endif
+#endif
