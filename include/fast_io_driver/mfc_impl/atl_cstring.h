6d68399a00a818d73d334cd0cdc87d420292e5e1
expnkx <unlvsur@live.com>
2021-11-08 03:08:58 -0500

    use cross_code_cvt for atl cstring. the problem is that it sometimes is wide sometimes is ascii for CString itself. Fixed

--- include/fast_io_driver/mfc_impl/atl_cstring.h
+++ include/fast_io_driver/mfc_impl/atl_cstring.h
@@ -4,9 +4,9 @@ namespace fast_io
 {
 
 template<std::integral ch_type,typename traits_type>
-inline constexpr basic_io_scatter_t<ch_type> print_alias_define(io_alias_t,::ATL::CStringT<ch_type, traits_type> const& atl_mfccstr) noexcept
+inline constexpr cross_code_cvt_t<ch_type> print_alias_define(::fast_io::io_alias_t,::ATL::CStringT<ch_type, traits_type> const& atl_mfccstr) noexcept
 {
-	return {static_cast<ch_type const*>(atl_mfccstr),static_cast<std::size_t>(atl_mfccstr.GetLength())};
+	return {{static_cast<ch_type const*>(atl_mfccstr),static_cast<std::size_t>(atl_mfccstr.GetLength())}};
 }
 
 }
