d300cb9b40fbd2695d4615bc7f0d7997abde19a0
expnkx <unlvsur@live.com>
2021-11-09 01:10:59 -0500

    should run 16 rounds for basic_concat_buffer.cc

--- fuzzing/0003.concat/basic_concat_buffer.cc
+++ fuzzing/0003.concat/basic_concat_buffer.cc
@@ -5,6 +5,7 @@ extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const* ptr,std::size_t n) noe
 {
 	::fast_io::details::basic_concat_buffer<char> buffer;
 	auto oref{io_strlike_ref(fast_io::io_alias,buffer)};
-	write(oref,ptr,ptr+n);
+	for(std::size_t i{};i!=16;++i)
+		write(oref,ptr,ptr+n);
         return 0;
 }
